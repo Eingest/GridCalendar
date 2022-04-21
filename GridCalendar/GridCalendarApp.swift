917c68382b4ec59d8554bc250b96201c3ba3d04c
Eingest <ak0805@live.de>
2022-04-21 16:13:17 +0200

    Initial Commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Andreas Kiesel on 21.04.22.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
